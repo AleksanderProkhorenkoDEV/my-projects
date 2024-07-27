a787639f83294a521f294ba4890a7ade94731e68
“Aleksander” <aprokhorenkodev@gmail.com>
2024-07-27 09:57:59 +0200

    feat: add the i18n example to test in the app

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,16 +1,8 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    i18n: {
-        locales: ['en-US', 'fr', 'es'],
-        defaultLocale: 'es',
-        domains: [
-            {
-                domain: 'example.com',
-                defaultLocale: 'es',
-            },
-        ],
-    },
+const nextConfig = {};
 
-};
-
-export default nextConfig;
+export default withNextIntl(nextConfig);
