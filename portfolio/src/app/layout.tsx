c5467a51db7b0b97a971ed380196c91d74bc5705
“Aleksander” <aprokhorenkodev@gmail.com>
2024-07-27 09:50:31 +0200

    style: tab code

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,17 +1,13 @@
+import { roboto } from "@/assets/fonts";
 import type { Metadata } from "next";
 import "./globals.css";
-import { roboto } from "@/assets/fonts";
 
 export const metadata: Metadata = {
   title: "APROKHORENKODEV",
   description: "Porfolio de Aleksander",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{ children: React.ReactNode;}>) {
   return (
     <html lang="es">
       <body className={`bg-main-bg ${roboto.className}`} >{children}</body>
