7d2722a1625633d15887134b9f8409856966220a
“Aleksander” <aprokhorenkodev@gmail.com>
2024-08-06 20:01:56 +0200

    chore: add configuration to nextui

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -1,19 +1,24 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Asegúrate de incluir el path a NextUI
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-
+      // Puedes agregar configuraciones adicionales aquí si lo deseas
     },
-    colors:{
-      "main-bg": "#E1E1DD"
-    }
+    colors: {
+      "main-bg": "#E1E1DD",
+    },
   },
-  plugins: [],
+  darkMode: "class", // Si estás usando el modo oscuro
+  plugins: [nextui()], // Incluye el plugin NextUI
 };
+
 export default config;
