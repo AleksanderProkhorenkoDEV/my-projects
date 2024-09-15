eb72d1df9fbefe46b15e78253777ac66492d80a8
“Aleksander” <aprokhorenkodev@gmail.com>
2024-09-15 12:16:16 +0200

    feat: finish hero section in projects

--- portfolio/src/app/[locale]/projects/page.tsx
+++ portfolio/src/app/[locale]/projects/page.tsx
@@ -3,22 +3,23 @@ import { bebas, montserrat } from '@/assets/fonts';
 import { useTranslations } from 'next-intl';
 import { ProjectPageProps } from '@/types';
 import { Velustro } from "uvcanvas"
+import TextGenerateEffect from '@/components/anima-text/animate-text';
 
 
 
 
 export default function ProjectPage({ params: { locale } }: ProjectPageProps) {
     unstable_setRequestLocale(locale);
-    const t = useTranslations('HomePage');
+    const t = useTranslations('projects');
     return (
         <main className={`w-full flex flex-col gap-8 pb-8 ${montserrat.className}`}>
             <section className='w-full h-screen flex flex-col items-start justify-end gap-4 overflow-hidden relative'>
                 <div className='w-[100rem] h-1/2 rotate-[23deg] transform translate-x-[-10%] absolute left-[0px] top-[0px] rounded-br-full rounded-tr-full overflow-hidden '>
                     <Velustro />
                 </div>
-                <div className='border-indigo-800 border-2 h-1/2 w-[35rem] ml-2 p-4'>
-                    <h1 className={`uppercase ${bebas.className} text-7xl`}>Projects Zone</h1>
-                    <p>Aqui podréis ver mi arte</p>
+                <div className='h-1/2 w-[35rem] p-4 flex flex-col items-center justify-center'>
+                    <TextGenerateEffect words={t('hero-title')} className={`uppercase ${bebas.className} text-6xl w-3/4`}></TextGenerateEffect>
+                    <TextGenerateEffect words={t('hero-text')} className='text-end w-3/4 text-2xl'></TextGenerateEffect>
                 </div>
             </section>
         </main>
