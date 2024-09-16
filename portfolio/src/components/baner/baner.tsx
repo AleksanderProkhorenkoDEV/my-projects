9f43d156914de99511eae51d162f823d8df3ef16
“Aleksander” <aprokhorenkodev@gmail.com>
2024-09-16 12:36:35 +0200

    feat: add new componentes for the app, banner and card

---
+++ portfolio/src/components/baner/baner.tsx
@@ -0,0 +1,25 @@
+import { useTranslations } from "next-intl";
+import { bebas } from "@/assets/fonts"
+import Link from "next/link"
+
+const Baner = () => {
+    const t = useTranslations('banner');
+    return (
+        <div className="w-full flex items-center justify-center">
+            <section className="border-2 border-transparent flex flex-col gap-6 items-start justify-start p-6 bg-gradient-to-r from-dark-main-color via-indigo-700 to-main-color text-white rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl w-11/12">
+                <h3 className={`text-5xl font-extrabold tracking-tight ${bebas.className} leading-tight tracking-[.5rem]`}>
+                    {t("title")}
+                </h3>
+                <p className="text-pretty text-lg leading-relaxed">
+                    {t("message")}
+                </p>
+                <Link href={'/contact'} className="shadow-[inset_0_0_0_2px_#fff] tracking-wide hover:shadow-[inset_0_0_0_2px_#1040b0] hover:scale-105 transform transition-all duration-300 text-white px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-white hover:text-main-color dark:text-neutral-200">
+                    {t("contacta")}
+                </Link>
+            </section>
+
+        </div>
+    )
+}
+
+export default Baner
