1bae0d2fc7d8cbca0eb1a51e44a6271dc325b228
“Aleksander” <aprokhorenkodev@gmail.com>
2024-09-17 14:23:59 +0200

    feat: add props

--- portfolio/src/components/card/card.tsx
+++ portfolio/src/components/card/card.tsx
@@ -1,7 +1,9 @@
-import { Card, CardBody, Image, Button, Slider } from "@nextui-org/react";
+import { Cards } from "@/types";
+import { Card, CardBody, Image} from "@nextui-org/react";
+import Link from "next/link";
 import React from "react";
 
-const OwnCard = () => {
+const OwnCard = ({image, title, description, github, link}: Cards) => {
 
     return (
         <Card
@@ -13,11 +15,11 @@ const OwnCard = () => {
                 <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
                     <div className="relative col-span-6 md:col-span-4">
                         <Image
-                            alt="Album cover"
+                            alt={title}
                             className="object-cover"
                             height={200}
                             shadow="md"
-                            src="https://nextui.org/images/album-cover.png"
+                            src={image}
                             width="100%"
                         />
                     </div>
@@ -25,13 +27,14 @@ const OwnCard = () => {
                     <div className="flex flex-col col-span-6 md:col-span-8">
                         <div className="flex justify-between items-start">
                             <div className="flex flex-col gap-0">
-                                <h3 className="font-semibold text-foreground/90">Daily Mix</h3>
-                                <p className="text-small text-foreground/80">12 Tracks</p>
-                                <h1 className="text-large font-medium mt-2">Frontend Radio</h1>
+                                <h2 className="text-large font-medium mt-2">{title}</h2>
+                                <p className="text-small text-foreground/80">{description}</p>
                             </div>
-
                         </div>
-
+                        <div>
+                            <Link href={github}>Github</Link>
+                            <Link href={link}>View project</Link>
+                        </div>
                     </div>
                 </div>
             </CardBody>
