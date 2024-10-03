84eaf7599e2a1a41944502e86c8854ca7b1fde0d
“Aleksander” <aprokhorenkodev@gmail.com>
2024-10-03 15:32:29 +0200

    feat: create second tab

---
+++ portfolio/src/components/tabs/tab2/estudios.tsx
@@ -0,0 +1,11 @@
+import { Grid } from "@/components/grid-layout/grid-layout";
+import cards from "@/components/grid-layout/Projects/Cards";
+
+const Formacion = async () => {
+
+    return (
+        <Grid cards={cards} />
+    )
+}
+
+export default Formacion
