f2400470033f2a0b87c10c92686bc63cd7b5b442
“Aleksander” <aprokhorenkodev@gmail.com>
2024-10-07 12:29:30 +0200

    feat: change the style in download button and delete border in all the buttons

--- portfolio/src/components/baner/baner.tsx
+++ portfolio/src/components/baner/baner.tsx
@@ -7,17 +7,17 @@ const Baner = () => {
     return (
         <div className="w-full flex items-center justify-center">
             <section className=" flex flex-col gap-6 items-start justify-start p-6 bg-gradient-to-r from-dark-main-color via-indigo-700 to-main-color text-white rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl w-11/12">
-                <h3 className={`text-5xl font-extrabold tracking-tight ${bebas.className} leading-tight tracking-[.70rem]`}>
+                <h3 className={`text-5xl font-extrabold tracking-tight ${bebas.className} leading-tight tracking-[.79rem]`}>
                     {t("title")}
                 </h3>
                 <p className="text-pretty text-lg leading-relaxed">
                     {t("message")}
                 </p>
                 <div className="flex gap-4">
-                    <Link href={'/contact'} className="shadow-[inset_0_0_0_2px_#fff] tracking-wide hover:shadow-[inset_0_0_0_2px_#1040b0] hover:scale-105 transform transition-all duration-300 text-white px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-white hover:text-main-color dark:text-neutral-200">
+                    <Link href={'/contact'} className="shadow-[inset_0_0_0_2px_#fff] tracking-wide  hover:scale-105 transform transition-all duration-300 text-white px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-white hover:text-main-color dark:text-neutral-200">
                         {t("contacta")}
                     </Link>
-                    <a href={'/assets/cv/Spanish_Full_Stack_cv.pdf'} download="aleksander_cv.pdf" className="shadow-[inset_0_0_0_2px_#fff] tracking-wide hover:shadow-[inset_0_0_0_2px_#1040b0] hover:scale-105 transform transition-all duration-300 text-white px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-white hover:text-main-color dark:text-neutral-200">
+                    <a href={'/assets/cv/Spanish_Full_Stack_cv.pdf'} download="aleksander_cv.pdf" className="bg-white text-main-color  tracking-wide hover:shadow-[inset_0_0_0_2px_#fff] hover:scale-105 transform transition-all duration-300 px-12 py-4 rounded-full tracking-widest uppercase font-bold hover:bg-transparent hover:text-white dark:text-neutral-200">
                         {t("cv")}
                     </a>
                 </div>
