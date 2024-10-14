f4f35b75ff7441cc8e6fa0286dbdc11282593521
“Aleksander” <aprokhorenkodev@gmail.com>
2024-10-14 20:40:17 +0200

    fix: change css

--- portfolio/src/components/baner/baner.tsx
+++ portfolio/src/components/baner/baner.tsx
@@ -5,9 +5,9 @@ import Link from "next/link"
 const Baner = () => {
     const t = useTranslations('banner');
     return (
-        <div className="w-full flex items-center justify-center">
-            <section className=" flex flex-col gap-6 items-start justify-start p-6 bg-gradient-to-r from-dark-main-color via-indigo-700 to-main-color text-white rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl w-11/12">
-                <h3 className={`text-5xl font-extrabold tracking-tight ${bebas.className} leading-tight tracking-[.79rem]`}>
+        <div className="w-full flex items-center justify-center bg-gradient-to-r from-dark-main-color via-indigo-700 to-main-color text-white rounded-lg shadow-lg ">
+            <section className="w-11/12 flex flex-col gap-6 items-start justify-start p-6 ">
+                <h3 className={`text-5xl font-extrabold tracking-tight ${bebas.className} leading-tight tracking-[0.5rem]`}>
                     {t("title")}
                 </h3>
                 <p className="text-pretty text-lg leading-relaxed">
