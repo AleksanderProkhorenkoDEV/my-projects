338655369c5119e327720d434c74f243d450f339
“Aleksander” <aprokhorenkodev@gmail.com>
2024-10-16 11:58:41 +0200

    feat: add media querys for hero section in main page

--- portfolio/src/app/[locale]/page.tsx
+++ portfolio/src/app/[locale]/page.tsx
@@ -16,11 +16,11 @@ export default function HomePage({ params: { locale } }: ProjectPageProps) {
     return (
         <main className='w-full flex flex-col gap-8 pb-8'>
             <section className='w-full h-screen flex flex-col items-center justify-center gap-4'>
-                <h1 className={`w-[75rem] uppercase text-7xl ${bebas.className} tracking-wider z-20`}>{t('title')}</h1>
-                <div className='w-[80rem] h-1/2'>
-                    <Novatrix />
+                <h1 className={`w-[75rem] uppercase text-7xl ${bebas.className} tracking-wider z-20 1k3:text-6xl 1k2:text-4xl 1k2:w-[65rem] 1k:text-2xl 1k:w-[55rem]`}>{t('title')}</h1>
+                <div className='w-[80rem] h-1/2 1k3:w-[70rem] 1k2:w-[60rem] 1k:w-[45rem]'>
+                    <Novatrix/>
                 </div>
-                <p className={`w-[75rem] text-end uppercase text-xl ${montserrat.className} tracking-wider `}>Aleksander Trujillo Prokhorenko</p>
+                <p className={`w-[75rem] text-end uppercase text-xl ${montserrat.className} tracking-wider 1k3:text-lg 1k2:text-base 1k2:w-[65rem] 1k:w-[55rem]`}>Aleksander Trujillo Prokhorenko</p>
                 <article className='flex justify-between text-white w-full bg-[#2D2A32] py-6 px-10 uppercase mt-8'>
                     <p>{t('location')}</p>
                     <div className='flex gap-2 justify-center items-center'><Image src={"/assets/icons/world.svg"} width={20} height={20} alt='World icon' /> <p>{t('www')}</p></div>
