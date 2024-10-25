ed2b3b5d58a3bc5123b0820836ff63c412aa0401
“Aleksander” <aprokhorenkodev@gmail.com>
2024-10-25 16:38:08 +0200

    feat: make responsive hero section

--- portfolio/src/app/[locale]/projects/page.tsx
+++ portfolio/src/app/[locale]/projects/page.tsx
@@ -8,8 +8,9 @@ import Stack from '@/components/tabs/tab3/Stack';
 import { Tabs } from '@/components/tabs/tabs';
 import Baner from '@/components/baner/baner';
 import OwnCard from '@/components/card/card';
-import { Velustro } from "uvcanvas"
+import { Velustro } from "uvcanvas";
 import { Suspense } from 'react';
+import Image from 'next/image';
 
 export default async function ProjectPage({ params: { locale } }: ProjectPageProps) {
     unstable_setRequestLocale(locale);
@@ -19,12 +20,22 @@ export default async function ProjectPage({ params: { locale } }: ProjectPagePro
     return (
         <main className={`w-full flex flex-col gap-8 pb-8 ${montserrat.className}`}>
             <section className='w-full h-screen flex flex-col items-start justify-end gap-4 overflow-hidden relative'>
-                <div className='w-[100rem] h-1/2 rotate-[23deg] transform translate-x-[-10%] absolute left-[0px] top-[0px] rounded-br-full rounded-tr-full overflow-hidden '>
+                <div className='w-[100rem] h-1/2 rotate-[23deg] transform translate-x-[-10%] absolute left-0 top-0 rounded-br-full rounded-tr-full overflow-hidden 1k:w-full 1k:h-full 1k:transform-none 1k:translate-x-0 1k:rotate-0 1k:relative 1k:left-0 1k:top-0 1k:rounded-none'>
                     <Velustro />
                 </div>
-                <div className='h-1/2 w-[35rem] p-4 flex flex-col items-center justify-center'>
+                <div className='h-1/2 w-[35rem] p-4 flex flex-col items-center justify-center 1k:hidden'>
                     <TextGenerateEffect words={t('hero-title')} className={`uppercase ${bebas.className} text-6xl w-3/4`}></TextGenerateEffect>
                     <TextGenerateEffect words={t('hero-text')} className='text-end w-3/4 text-2xl'></TextGenerateEffect>
+                </div>
+                <div className='hidden h-1/2  p-4 flex 1k:flex-col 1k:items-center  1k:justify-center 1k:flex 1k:gap-8 1k:absolute 1k:left-[35%] 1k:top-[25%] 700k:left-[28%] 600k:left-[20%] 500k:left-[18%] 400k:left-[16%] 300k:left-[7%]'>
+                    <TextGenerateEffect words={t('hero-title')} className={`uppercase ${bebas.className} text-6xl 600k:text-4xl 300k:text-xl`}></TextGenerateEffect>
+                    <Image
+                        src={'/assets/images/logo.png'}
+                        width={250}
+                        height={250}
+                        alt='logo aleksander'
+                        className='rotate-horizontal' />
+                    <TextGenerateEffect words={t('hero-text')} className='text-2xl 600k:text-xl 300k:text-lg'></TextGenerateEffect>
                 </div>
             </section>
             <Baner />
@@ -47,9 +58,9 @@ export default async function ProjectPage({ params: { locale } }: ProjectPagePro
             <section className='h-screen w-11/12 mx-auto mb-8'>
                 <Tabs
                     tabs={[
-                        { title: "Experiencia", value: "1", content: <Experiencia locale={locale}/> },
+                        { title: "Experiencia", value: "1", content: <Experiencia locale={locale} /> },
                         { title: "Formación", value: "2", content: <Formacion /> },
-                        { title: "Stack teconlogico", value: "3", content: <Stack />}
+                        { title: "Stack teconlogico", value: "3", content: <Stack /> }
                     ]}
                     containerClassName='h-min w-full'
                     tabClassName="px-6 py-2 bg-gray-100 hover:bg-gray-200 dark:hover:bg-zinc-700 transition-all duration-300 ease-in-out rounded-t-md"
