af6ca253717abbc53d609622448a600e87590674
“Aleksander” <aprokhorenkodev@gmail.com>
2024-10-25 16:37:52 +0200

    feat: make responsive banner

--- portfolio/src/components/baner/baner.tsx
+++ portfolio/src/components/baner/baner.tsx
@@ -7,13 +7,13 @@ const Baner = () => {
     return (
         <div className="w-full flex items-center justify-center bg-gradient-to-r from-dark-main-color via-indigo-700 to-main-color text-white rounded-lg shadow-lg ">
             <section className="w-11/12 flex flex-col gap-6 items-start justify-start p-6 ">
-                <h3 className={`text-5xl font-extrabold tracking-tight ${bebas.className} leading-tight tracking-[0.5rem]`}>
+                <h3 className={`text-5xl font-extrabold tracking-tight ${bebas.className} leading-tight tracking-[0.35rem] 700k:text-2xl`}>
                     {t("title")}
                 </h3>
                 <p className="text-pretty text-lg leading-relaxed">
                     {t("message")}
                 </p>
-                <div className="flex gap-4">
+                <div className="flex gap-4 700k:flex-wrap">
                     <Link href={'/contact'} className="shadow-[inset_0_0_0_2px_#fff] tracking-wide  hover:scale-105 transform transition-all duration-300 text-white px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-white hover:text-main-color dark:text-neutral-200">
                         {t("contacta")}
                     </Link>
